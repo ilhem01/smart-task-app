2ac1832f648a150759ea73ff88b54e65753a3a3d
ilhem <ilhem.djebbi.it@gmail.com>
2026-04-04 01:33:46 +0200

    fix: add CORS config

--- auth-service/src/main/java/com/smarttask/auth/config/SecurityBeansConfig.java
+++ auth-service/src/main/java/com/smarttask/auth/config/SecurityBeansConfig.java
@@ -2,6 +2,7 @@ package com.smarttask.auth.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -14,6 +15,7 @@ public class SecurityBeansConfig {
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
+                .cors(Customizer.withDefaults())
                 .csrf(csrf -> csrf.disable())
                 .httpBasic(httpBasic -> httpBasic.disable())
                 .formLogin(form -> form.disable())
